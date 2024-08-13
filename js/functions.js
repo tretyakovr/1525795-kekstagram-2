a99b6d0bd52aa90ecfe4ce0f30ec7a5841de03de
rtretyakov <rtretyakov@outlook.com>
2024-08-14 07:45:10 +1000

    Добавил вызовы функций

--- js/functions.js
+++ js/functions.js
@@ -25,3 +25,22 @@ function getDigits (symbols) {
 
   return resultStr === '' ? NaN : +resultStr;
 }
+
+
+checkStrLength('проверяемая строка', 20); // true
+checkStrLength('проверяемая строка', 18); // true
+checkStrLength('проверяемая строка', 10); // false
+
+isPalindrom('топот'); // true
+isPalindrom('ДовОд'); // true
+isPalindrom('Кекс'); // false
+isPalindrom('Лёша на полке клопа нашёл '); // true
+
+getDigits('2023 год'); // 2023
+getDigits('ECMAScript 2022'); // 2022
+getDigits('1 кефир, 0.5 батона'); // 105
+getDigits('агент 007'); // 7
+getDigits('а я томат'); // NaN
+getDigits(2023); // 2023
+getDigits(-1); // 1
+getDigits(1.5); // 15
