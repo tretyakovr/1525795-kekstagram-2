d8a46d6813f416b290eb60680c90f754a7dae497
R Tretyakov <tretyakov.rv@mail.ru>
2024-08-18 08:23:40 +1000

    module5-task2

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,8 @@
-function checkStrLength (str, maxLength) {
+function checkStrLength(str, maxLength) {
   return str.length <= maxLength;
 }
 
-function isPalindrom (str) {
+function isPalindrom(str) {
   str = str.toLowerCase().replaceAll(' ', '');
 
   let targetStr = '';
@@ -15,32 +15,54 @@ function isPalindrom (str) {
 }
 
 
-function getDigits (symbols) {
+function getDigits(symbols) {
   const str = symbols.toString();
   let resultStr = '';
 
-  for (let i = 0; i <= str.length - 1; i ++) {
+  for (let i = 0; i <= str.length - 1; i++) {
     resultStr += isNaN(parseInt(str[i], 10)) ? '' : parseInt(str[i], 10);
   }
 
   return resultStr === '' ? NaN : +resultStr;
 }
 
+function checkMeetLength(workStart, workEnd, meetStart, meetLength) {
+  let [startHour, startMinutes] = workStart.split(':');
+  let [endHour, endMinutes] = workEnd.split(':');
+  let [meetHour, meetMinutes] = meetStart.split(':');
 
-checkStrLength('проверяемая строка', 20); // true
-checkStrLength('проверяемая строка', 18); // true
-checkStrLength('проверяемая строка', 10); // false
+  let result =
+    // Начало встречи ранее начала рабочего дня?
+    (+meetHour * 60 + +meetMinutes) < (+startHour * 60 + +startMinutes) ||
+    // Начало встречи позднее конца рабочего дня?
+    (+meetHour * 60 + +meetMinutes) > (+endHour * 60 + +endMinutes) ||
+    // Начало встречи в пределах рабочего дня, но не позднее конца?
+    (+meetHour * 60 + +meetMinutes) + +meetLength <= (+endHour * 60 + +endMinutes);
 
-isPalindrom('топот'); // true
-isPalindrom('ДовОд'); // true
-isPalindrom('Кекс'); // false
-isPalindrom('Лёша на полке клопа нашёл '); // true
+  return result;
+}
 
-getDigits('2023 год'); // 2023
-getDigits('ECMAScript 2022'); // 2022
-getDigits('1 кефир, 0.5 батона'); // 105
-getDigits('агент 007'); // 7
-getDigits('а я томат'); // NaN
-getDigits(2023); // 2023
-getDigits(-1); // 1
-getDigits(1.5); // 15
+console.log(checkMeetLength('08:00', '17:30', '14:00', 90)); // true
+console.log(checkMeetLength('8:0', '10:0', '8:0', 120));     // true
+console.log(checkMeetLength('08:00', '14:30', '14:00', 90)); // false
+console.log(checkMeetLength('14:00', '17:30', '08:0', 90));  // false
+console.log(checkMeetLength('8:00', '17:30', '08:00', 900)); // false
+
+
+// checkStrLength('проверяемая строка', 20); // true
+// checkStrLength('проверяемая строка', 18); // true
+// checkStrLength('проверяемая строка', 10); // false
+
+// isPalindrom('топот'); // true
+// isPalindrom('ДовОд'); // true
+// isPalindrom('Кекс'); // false
+// isPalindrom('Лёша на полке клопа нашёл '); // true
+
+// getDigits('2023 год'); // 2023
+// getDigits('ECMAScript 2022'); // 2022
+// getDigits('1 кефир, 0.5 батона'); // 105
+// getDigits('агент 007'); // 7
+// getDigits('а я томат'); // NaN
+// getDigits(2023); // 2023
+// getDigits(-1); // 1
+// getDigits(1.5); // 15
