1559815d7a92488b9ae62c601c9d8e0f8689d7d7
rtretyakov <rtretyakov@outlook.com>
2024-08-20 11:37:18 +1000

    module7-task1

---
+++ js/get-photos.js
@@ -0,0 +1,43 @@
+import { getGeneratedId } from './utils';
+import {
+  getRandomInteger,
+  getUniqueRandomInteger
+} from './random';
+import {
+  getRandomDescription,
+  getRandomNickName,
+  getCommentsList
+} from './data';
+
+const MIN_COMMENTS_COUNT = 0;
+const MAX_COMMENTS_COUNT = 30;
+const MIN_LIKES_COUNT = 15;
+const MAX_LIKES_COUNT = 200;
+const PHOTOS_COUNT = 25;
+
+// Генерация комментария
+const commentId = getGeneratedId();
+const getComments = () => ({
+  id: commentId(),
+  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+  message: getCommentsList().join(' '),
+  name: getRandomNickName(),
+});
+
+// Функция генерации фото
+const photoId = getGeneratedId();
+const photoNum = getUniqueRandomInteger(1, 25);
+
+const getPhoto = () => ({
+  id: photoId(),
+  url: `photos/${photoNum()}.jpg`,
+  description: getRandomDescription(),
+  likes: getRandomInteger(MIN_LIKES_COUNT, MAX_LIKES_COUNT),
+  comments: Array.from({
+    length: getRandomInteger(MIN_COMMENTS_COUNT, MAX_COMMENTS_COUNT)
+  }, getComments),
+});
+
+export function getPhotos() {
+  return Array.from({ length: PHOTOS_COUNT }, getPhoto);
+}
