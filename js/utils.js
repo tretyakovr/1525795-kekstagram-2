8205ceb2ca41a18dfebb57bdedf9d3695f35103f
R Tretyakov <tretyakov.rv@mail.ru>
2024-08-28 00:54:46 +1000

    module9-task1

--- js/utils.js
+++ js/utils.js
@@ -7,3 +7,19 @@ export function getGeneratedId() {
     return lastId;
   };
 }
+
+
+function countItems(arr, item) {
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === item) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+export function isDuplicates(arr) {
+  return !(arr.every((item) => { return countItems(arr, item) === 1 }));
+}
